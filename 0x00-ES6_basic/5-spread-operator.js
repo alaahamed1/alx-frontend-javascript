616f9f6428f14742b7e9c2241fd8889852e13a13
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-10-01 18:04:22 +0300

    lint issues

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,12 +1,3 @@
-#!/usr/bin/env node
-
-/**
- * concatArrays- main function
- * @param {*} array1 
- * @param {*} array2 
- * @param {*} string 
- * @returns 
- */
 export default function concatArrays(array1, array2, string) {
-    return [...array1, ...array2, ...string];
-  }
+  return [...array1, ...array2, ...string];
+}
