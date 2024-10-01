c78cbd81af2112e4fa94e1328bf865777af614c2
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-10-01 17:52:14 +0300

    lint issues

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,8 +4,8 @@ export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
     let task = true;
-    let task2 = false;
-    }
+   let task2 = false;
+  }
 
   return [task, task2];
 }
