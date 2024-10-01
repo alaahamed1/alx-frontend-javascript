5823988de6956c37b4726bc5f40aaee972e630d5
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-10-01 18:15:32 +0300

    lint issues

---
+++ 0x00-ES6_basic/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides: [
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      },
+    ],
+  };
